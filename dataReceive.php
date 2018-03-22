a39d51dbfbaf5345f88b27ffcdb725fb360af4a1
Amol Kedar <akedar@zerionsoftware.com>
2018-03-22 01:44:09 -0400

    added updates to add multi level forms

--- dataReceive.php
+++ dataReceive.php
@@ -7,30 +7,64 @@ $tableName = $_GET['tablename'];
 //get the inputs of the incoming JSON into a array
 $data = json_decode(file_get_contents('php://input'), true);
 $incomingData = $data["0"]["record"];
+
 echo $tableName. "\n";
 //print_r($incomingData). "\n";
-
 $dataColumns = array_keys($incomingData); 
-
 $dbObject = new DB_Object();
 
-$tableExists =  $dbObject->table_exists($tableName);
-// print_r(count($tableExists));
-// echo "count was " . "\n";
-//echo "printing table exists" . json_encode($tableExists);
 
-if (count($tableExists) > 0) {
-	echo "Table exists";
-	echo "\n";
-	$dbObject->save($tableName, $incomingData);	
+$finalColumns = array();
+$sendToFlatten = array();
+$flattenedColumns = array();
 
-} else {
-	echo "Table does not exists";
-	echo "\n";
-	$dbObject->create($tableName, $incomingData);		
-
+foreach ($incomingData as $key => $value) {
+		
+		if(is_string($value)) {
+//			echo "key is String ". $key ."\n";	
+			$finalColumns[$key] = $value;
+		} else {
+			echo "Count of Key is Array ". sizeof($value) ."\n";	
+			//$sendToFlatten[$key] = $value;			
+			for ($i=0; $i < sizeof($value); $i++) { 
+				$sendToFlatten[] = flatten($key, $value[$i]);					
+			}						
+		}
+		
 }
 
+foreach ($sendToFlatten as $key => $value) {
+	$flattenedColumns[] = array_merge($finalColumns, $value);
+}
+
+for ($i=0; $i < sizeof($flattenedColumns); $i++) { 
+	//print_r($flattenedColumns[$i]);
+	$tableExists =  $dbObject->table_exists($tableName);
+	if (count($tableExists) > 0) {
+		echo "Table exists";
+		echo "\n";
+		$dbObject->save($tableName, $flattenedColumns[$i]);	
+
+	} else {
+		echo "Table does not exists";
+		echo "\n";
+		$dbObject->create($tableName, $flattenedColumns[$i]);		
+
+	}
+}
+
+
+function flatten($columnName, array $array) {
+	$branch = array();
+	foreach ($array as $key => $value) {
+		if ($key == 'record') {
+				foreach ($value as $key => $value) {
+					$branch[$columnName."_".$key] =  $value;
+				}
+		}
+	}
+	return $branch;
+}
 
 
 
